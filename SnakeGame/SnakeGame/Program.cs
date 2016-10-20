5077ffaef19a9690c6b31ebdaf94c7fbf9a96c7f
model25 <model25@pochta.ru>
2016-10-20 12:49:51 +0300

    Hello SnakeGame

---
+++ SnakeGame/SnakeGame/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 20.10.2016
+ * Time: 12:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SnakeGame
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+					
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Hello to SnakeGame !!!");
+			Console.ReadKey(true);
+		}
+	}
+}
