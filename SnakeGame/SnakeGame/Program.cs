340a47fa3b411b96c9935630357665b901bef959
model25 <model25@pochta.ru>
2016-10-20 13:08:50 +0300

    new function DrawSymbol

--- SnakeGame/SnakeGame/Program.cs
+++ SnakeGame/SnakeGame/Program.cs
@@ -14,11 +14,28 @@ namespace SnakeGame
 	{
 		public static void Main(string[] args)
 		{
-					
+			int x1 = 1;
+			int y1 = 3;
+			char symbol1 = '*';
+			
+		    int x2 = 4;
+			int y2 = 5;
+			char symbol2 = '#';			
+			Console.Write("Hello to SnakeGame !!!");
+			
+
 			// TODO: Implement Functionality Here
 			
-			Console.Write("Hello to SnakeGame !!!");
+			DrawSymbol(x1,y1,symbol1);
+			DrawSymbol(x2,y2,symbol2);
+			
 			Console.ReadKey(true);
+		}
+		
+		static void DrawSymbol(int x,int y,char symbol)
+		{
+			Console.SetCursorPosition(x,y);
+			Console.Write(symbol);
 		}
 	}
 }
